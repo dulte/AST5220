d238098247275bb6e786409f824de6e3a97f5cc6
Daniel <daniel.heinesen@sf-nett.no>
2019-05-01 15:49:59 +0200

    Small notes in report

--- milestone3/analysis/milestone3.py
+++ milestone3/analysis/milestone3.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 
-sns.set_style("darkgrid")
-sns.set_context("talk")
+#sns.set_style("darkgrid")
+#sns.set_context("talk")
 
 def read_file(name):
     y = []
